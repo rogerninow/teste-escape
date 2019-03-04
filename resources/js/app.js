ec169b19a355210c34f55144a88c0d96f3fe4d61
Róger <rogerninow@gmail.com>
2019-03-04 17:17:14 -0300

    Commit final do teste.

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('noticias-create',  require('./components/noticias/Create.vue'));
+Vue.component('noticias-edit',    require('./components/noticias/Edit.vue'));
+Vue.component('noticias-index',   require('./components/noticias/Index.vue'));
+Vue.component('noticias-show',    require('./components/noticias/Show.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+require __DIR__ . '/noticias/noticias.php';
